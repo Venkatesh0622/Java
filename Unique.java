fc598fc397baac5c3734f426a9c0f3cc189f9eb3
Venkatesh <160704785+Venkatesh0622@users.noreply.github.com>
2024-02-26 22:05:30 +0530

    Eclipse
    Done

---
+++ Unique.java
@@ -0,0 +1,30 @@
+package practice;
+
+public class Unique 
+{
+	public static void main(String[] args) 
+	{
+		int []arr= {5,0,1,0,3,5,3,2,2};
+		int size=arr.length;
+		for(int i=0;i<size;i++)
+		{
+			boolean flag=true;
+			for(int j=0;j<size;j++)
+			{
+				if(i!=j)
+				{
+					if(arr[i]==arr[j])
+					{
+						flag=false;
+						break;
+					}
+				}
+			}
+			if(flag==true)
+			{
+				System.out.println(arr[i]);
+				return;
+			}
+		}
+	}
+}
